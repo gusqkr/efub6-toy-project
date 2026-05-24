3fc22a62d6c2bfd1279af6076adba35cfe761563
gusqkr <hyeon05@ewha.ac.kr>
2026-05-24 15:30:36 +0900

    구현: 글 작성 섹션 디자인 구현

--- twitter-clone/src/components/FeedHeader.jsx
+++ twitter-clone/src/components/FeedHeader.jsx
@@ -6,23 +6,25 @@ const HeaderContainer = styled.div`
   position: relative;
   justify-content: center;
   padding: 5px;
+  padding-bottom: 18px;
+  border-bottom: solid 1px #9a9a9a;
 `;
 
 const HeaderLeftText = styled.p`
   color: white;
   flex: 5;
   font-weight: 600;
-  font-size: 20px;
+  font-size: 15px;
   position: relative;
 
   &::after {
     content: "";
     position: absolute;
-    bottom: -24.5px;
+    bottom: -18px;
     left: 50%;
     transform: translateX(-50%);
-    width: 80px;
-    height: 6px;
+    width: 70px;
+    height: 4px;
     background-color: #32c5ff;
     border-radius: 20px;
   }
@@ -30,14 +32,14 @@ const HeaderLeftText = styled.p`
 const HeaderRightText = styled.p`
   color: gray;
   flex: 4;
-  font-size: 20px;
+  font-size: 15px;
 `;
 
 const SettingIcon = styled(FiSettings)`
   color: white;
   flex: 1;
   padding-top: 5px;
-  font-size: 20px;
+  font-size: 15px;
 `;
 
 const FeedHeader = () => {
--- twitter-clone/src/components/MainFeed.jsx
+++ twitter-clone/src/components/MainFeed.jsx
@@ -1,17 +1,19 @@
 import styled from "styled-components";
 import FeedHeader from "./FeedHeader";
+import PostingSection from "./PostingSection";
 
 const FeedContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 60%;
   border: solid 1px #9a9a9a;
-  padding: 10px;
+  padding: 10px 0px;
 `;
 function MainFeed() {
   return (
     <FeedContainer>
       <FeedHeader />
+      <PostingSection />
     </FeedContainer>
   );
 }
